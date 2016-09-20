20e5123966e1ee0e1241b0b9a226958aa673959b
Ellis Clayton <ellis@ellisclayton.info>
2016-09-20 11:38:29 +1000

    Readability yo

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ import mqtt from 'mqtt'
 const app = express()
 const port = process.env.PORT || 8080
 
-const options = {
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+const mqOptions = {
   port: process.env.MQTT_PORT,
   clientId: `mqttjs_${Math.random().toString(16).substr(2, 8)}`,
   username: process.env.MQTT_USER,
   password: process.env.MQTT_PASS
 }
 
-const client = mqtt.connect(process.env.MQTT_URL, options)
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+const client = mqtt.connect(process.env.MQTT_URL, mqOptions)
 
 client.on('connect', () => {
   client.publish('Connected', 'connected')
@@ -28,10 +28,7 @@ app.post('/', (req, res) => {
     return res.sendStatus(403)
   }
 
-  console.log(options)
-  client.publish('pr', '1', () => {
-    res.sendStatus(200)
-  })
+  client.publish('pr', '1', () => res.sendStatus(200))
 })
 
 app.listen(port)
