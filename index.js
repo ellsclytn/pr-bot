2f9df2c115a7a41e2db2a841db4e32030647b4b0
Ellis Clayton <ellis@ellisclayton.info>
2016-09-20 11:38:48 +1000

    Update connection topic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import express from 'express'
 import bodyParser from 'body-parser'
 import mqtt from 'mqtt'
@@ -19,8 +20,28 @@ const mqOptions = {
 const client = mqtt.connect(process.env.MQTT_URL, mqOptions)
 
 client.on('connect', () => {
-  client.publish('Connected', 'connected')
+  client.publish('connection', 'connected')
 })
+
+const scanPrs = (messages) => {
+  const prMessages = messages.filter((message) => (message.text.startsWith(':pr:') && !message.parent_user_id))
+  .map((message) => {
+    const { replies = [] } = message
+
+    message.replies = replies.map((reply) => messages.find(
+      (m) => (m.ts === reply.ts)
+    )).filter(r => r)
+    .filter((reply) => (
+      reply.text.startsWith(':merged:') || reply.text.startsWith(':closed:')
+    ))
+
+    return message
+  })
+
+  const unresponded = prMessages.filter(({replies}) => !replies.length).length
+
+  return unresponded
+}
 
 app.post('/', (req, res) => {
   if (req.body.token !== process.env.SLACK_TOKEN) {
@@ -28,8 +49,26 @@ app.post('/', (req, res) => {
     return res.sendStatus(403)
   }
 
-  client.publish('pr', '1', () => res.sendStatus(200))
+  axios.get('https://slack.com/api/channels.history', { params: {
+    token: process.env.SLACK_API_TOKEN,
+    channel: process.env.SLACK_CHANNEL,
+    count: 50
+  }}).then((response) => (response.data.messages))
+  .then((messages) => (scanPrs(messages)))
+  .then((unresponded) => {
+    if (unresponded && req.body.text === ':pr:') {
+      // New PR
+      client.publish('pr', '1', () => res.sendStatus(200))
+      return res.sendStatus(200)
+    } else if (!unresponded) {
+      // Cleared PRs
+      client.publish('pr', '1', () => res.sendStatus(200))
+      return res.sendStatus(200)
+    } else {
+      // No change
+      return res.sendStatus(200)
+    }
+  }).catch((err) => (console.error('Failed to fetch messages', err)))
 })
 
 app.listen(port)
-
